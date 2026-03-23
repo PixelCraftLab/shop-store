05032b90691557236daf3fe66965a57034d67189
PixelCraftLab <vishalgowdask65@gmail.com>
2026-03-23 18:09:32 +0530

    added bacground

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -2,13 +2,22 @@ import { Link } from "react-router-dom";
 
 function Landing() {
   return (
-    <div className="h-screen flex flex-col justify-center items-center bg-gray-100">
-      <h1 className="text-4xl font-bold mb-4">Welcome to Shop App </h1>
+    <>
+    <div 
+    style={{
+      backgroundImage: "url('https://t4.ftcdn.net/jpg/07/64/55/75/360_F_764557526_HlwV6rYpIxrfhrmlpTzl74INFoMmJs9Z.jpg')",
+      backgroundSize: "cover",
+
+    }}>
+    <div className="h-screen flex flex-col justify-center items-center ">
+      <h1 className="text-4xl font-bold mb-4 text-white backdrop-blur-2xl border-2 p-4 border-r-5 border-b-5 rounded-tl-[30px]">Welcome to Shop App </h1>
       <p className="mb-6 text-gray-600">
         Order products with just 25% initial payment
       </p>
 
-      <div className="space-x-4">
+      <div className="space-x-4"
+      
+      >
         <Link to="/login" className="bg-blue-500 text-white px-4 py-2 rounded">
           Login
         </Link>
@@ -20,6 +29,8 @@ function Landing() {
         </a>
       </div>
     </div>
+    </div>
+    </>
   );
 }
 
