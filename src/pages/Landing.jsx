c57708d64ed853810f64a2e54d81cc5d4fe2c1b1
PixelCraftLab <vishalgowdask65@gmail.com>
2026-03-24 00:26:38 +0530

    updated landing page style

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -10,22 +10,17 @@ function Landing() {
 
     }}>
     <div className="h-screen flex flex-col justify-center items-center ">
-      <h1 className="text-4xl font-bold mb-4 text-white backdrop-blur-2xl border-2 p-4 border-r-5 border-b-5 rounded-tl-[30px]">
+      <h1 className=" text-4xl font-bold mb-8 text-white backdrop-blur-2xl border-2 p-4 border-r-5 border-b-5 rounded-tl-[30px]">
         Welcome to Shop App </h1>
-      <p className="mb-6 text-gray-100 backdrop-blur-2xl rounded-2xl">
+      <p className=" mb-13 text-gray-100 backdrop-blur-2xl rounded-2xl">
         Order products with just 25% initial payment
       </p>
 
       <div className="space-x-4"
       
       >
-        <Link to="/login" className="bg-black text-white px-4 py-2 rounded">
-          Login
-        </Link>
-        <Link to="/register" className="bg-white text-black px-4 py-2 rounded">
-          Register
-        </Link>
-        <a href="/shop" className="text-blue-500 ">
+        
+        <a href="/shop" className="m-10 bg-white p-2 rounded-2xl text-black animate-pulse hover:bg-black hover:text-white transform transition-all duration-500 ease-in-ou">
         Go to Shop
         </a>
       </div>
--- src/pages/NavBar.jsx
+++ src/pages/NavBar.jsx
@@ -3,15 +3,30 @@ import { Link } from 'react-router'
 
 const NavBar = () => {
   return (
-    <div>
-        <Link to="/login" 
-        className="bg-black text-white px-4 py-2 rounded">
+    <div className="flex justify-between items-center px-14 py-2 ">
+      
+
+      <h1 className="text-black text-xl ">
+        Welcome to the smart stoor
+      </h1>
+
+      
+      <div className="flex gap-4">
+        <Link 
+          to="/login" 
+          className="bg-white text-gray-900 px-4 py-2 rounded-lg font-medium hover:bg-gray-200 transition"
+        >
           Login
         </Link>
-        <Link to="/register" 
-        className="bg-white text-black px-4 py-2 rounded">
+
+        <Link 
+          to="/register" 
+          className="bg-neutral-500 text-white px-4 py-2 rounded-lg font-medium hover:bg-blue-600 transition"
+        >
           Register
         </Link>
+      </div>
+
     </div>
   )
 }
