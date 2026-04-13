10ed764788be4350831a61abb39aa5f1c6faa7ab
PixelCraftLab <vishalgowdask65@gmail.com>
2026-04-13 17:05:54 +0530

    navbar styling sorrected

--- src/pages/NavBar.jsx
+++ src/pages/NavBar.jsx
@@ -1,34 +1,51 @@
-import React from 'react'
-import { Link } from 'react-router'
+import React from "react";
+import { Link } from "react-router";
 
 const NavBar = () => {
   return (
-    <div className="flex justify-between items-center px-14 py-2 ">
-      
+    <div
+      className="
+        fixed top-0 left-0 w-full z-50
+        backdrop-blur-xl bg-white/10 border-b border-white/20
+        shadow-lg shadow-black/20
+        flex justify-between items-center
+        px-6 md:px-14 py-3
+      "
+    >
 
-      <h1 className="text-black text-xl ">
-        Welcome to the smart stoor
+      <h1 className="text-white text-xl font-semibold tracking-wide">
+        Smart Store
       </h1>
 
-      
-      <div className="flex gap-4">
-        <Link 
-          to="/login" 
-          className="bg-white text-gray-900 px-4 py-2 rounded-lg font-medium hover:bg-gray-200 transition"
+
+      <div className="flex gap-4 items-center">
+
+        <Link
+          to="/login"
+          className="
+            px-4 py-2 rounded-lg text-white
+            hover:bg-white/10
+            transition duration-300 ease-in-out
+          "
         >
           Login
         </Link>
 
-        <Link 
-          to="/register" 
-          className="bg-neutral-500 text-white px-4 py-2 rounded-lg font-medium hover:bg-blue-600 transition"
+        <Link
+          to="/register"
+          className="
+            px-5 py-2 rounded-lg
+            bg-white text-black font-medium
+            hover:bg-gray-200
+            transition duration-300 ease-in-out
+          "
         >
           Register
         </Link>
+
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
